1a1119a27112de3196f1a75d47ae106fe7d2535e
vizafogo123 <tormar23@gmail.com>
2016-05-29 22:06:40 +0200

    kpok

--- Trees.py
+++ Trees.py
@@ -3,7 +3,6 @@ import urllib
 from pokpok.Formula import Formula
 from pokpok.Operation import operations
 from pyjamas.ui import HasAlignment
-from pyjamas.ui.DialogBox import DialogBox
 from pyjamas.ui.DialogWindow import DialogWindow
 from pyjamas.ui.DockPanel import DockPanel
 from pyjamas.ui.HTML import HTML
@@ -12,10 +11,8 @@ from pyjamas.ui.Image import Image
 from pyjamas.ui.Sink import Sink, SinkInfo
 from pyjamas.ui.Tree import Tree
 from pyjamas.ui.TreeItem import TreeItem
-from pyjamas import Window
 from pyjamas.ui.Button import Button
 
-from sink.Popups import MyDialog, MyDialogWindow
 
 
 def latex_to_url(latex):
@@ -53,19 +50,12 @@ class Trees(Sink):
         self.initWidget(self.panel)
 
     def onTreeItemSelected(self, item):
-        dlg = FormulaBuilder(operations)
+        def after(formula):
+            self.image1.setUrl(latex_to_url(formula.fill_with_placeholders().to_latex()))
+
+        dlg = FormulaBuilder(operations,after)
 
         dlg.show()
-        if item.children == []:
-            if not self.f.is_closed():
-                for op in [x["op"] for x in self.ops if item.userObject == x["proto"]]:
-                    self.f.add_one_op(op)
-                    self.image1.setUrl(latex_to_url(self.f.fill_with_placeholders().to_latex()))
-            else:
-
-                self.f = self.f.simplify()
-                # self.image2.setUrl(latex_to_url(self.f.to_latex()))
-                self.image2.setUrl(latex_to_url(self.f.to_cnf().to_latex()))
 
     def onTreeItemStateChanged(self, item):
         child = item.getChild(0)
@@ -124,28 +114,32 @@ def init():
 
 
 class FormulaBuilder(DialogWindow):
-    def __init__(self, operations):
+
+    def __init__(self, operations,after):
         DialogWindow.__init__(self, modal=True, close=True)
+        self.formula = Formula([])
+        self.after=after
 
         left = 100
         top = 100
 
         self.ops_with_buttons = [{"op": op, "button": Button(op.name, self)} for op in operations if op.available]
-        self.f = Formula([])
         dock = DockPanel()
-        dock.setSpacing(20)
+        dock.setSpacing(3)
 
         for owb in self.ops_with_buttons:
             dock.add(owb['button'], DockPanel.NORTH)
 
-        # dock.setCellHorizontalAlignment(closeButton, HasAlignment.ALIGN_RIGHT)
         dock.setWidth("300")
 
-        self.image = Image(latex_to_url(self.f.fill_with_placeholders().to_latex()))
+        self.image = Image(latex_to_url(self.formula.fill_with_placeholders().to_latex()))
         dock.add(self.image, DockPanel.EAST)
+        dock.setCellHorizontalAlignment(self.image, HasAlignment.ALIGN_TOP)
 
         self.doneButton=Button("Done",self)
         dock.add(self.doneButton,DockPanel.SOUTH)
+
+        dock.add(HTML("Normal text box:"),DockPanel.CENTER)
 
         self.setText("opkop")
         self.setPopupPosition(left, top)
@@ -159,6 +153,7 @@ class FormulaBuilder(DialogWindow):
     def onClick(self, sender):
         if sender==self.doneButton:
             self.hide()
+            self.after(self.formula)
 
         op=None
         for owb in self.ops_with_buttons:
@@ -166,6 +161,7 @@ class FormulaBuilder(DialogWindow):
                 self.setText(sender.getText())
                 op=owb['op']
 
-        if not self.f.is_closed():
-            self.f.add_one_op(op)
-            self.image.setUrl(latex_to_url(self.f.fill_with_placeholders().to_latex()))
+        if not self.formula.is_closed():
+            self.formula.add_one_op(op)
+            self.image.setUrl(latex_to_url(self.formula.fill_with_placeholders().to_latex()))
+
