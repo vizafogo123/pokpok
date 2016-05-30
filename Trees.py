16af9497602e9d0fef0cb92ff51a2a6be5d38db6
Torma Róbert <tormarobert@Tormas-MacBook-Pro.local>
2016-05-30 13:37:51 +0200

    pops ask ads'

--- Trees.py
+++ Trees.py
@@ -1,7 +1,7 @@
 import urllib
 
 from pokpok.Formula import Formula,AX_REG
-from pokpok.Operation import operations
+from pokpok.Operation import operations, Operation
 from pyjamas import Window
 from pyjamas.ui import HasAlignment
 from pyjamas.ui.DialogWindow import DialogWindow
@@ -63,7 +63,7 @@ class Trees(Sink):
             self.cnf=self.cnf.substitute(Formula([var]),formula)
             self.image2.setUrl(latex_to_url(self.cnf.to_latex()))
 
-        dlg = FormulaBuilder(operations,after,type='expr')
+        dlg = FormulaBuilder([op for op in operations if op.available and op.type==Operation.EXPRESSION],after,type='expr')
 
         dlg.show()
 
@@ -147,14 +147,15 @@ class FormulaBuilder(DialogWindow):
         dock.setCellHorizontalAlignment(self.image, HasAlignment.ALIGN_TOP)
 
         self.doneButton=Button("Done",self)
+        self.doneButton.setEnabled(False)
         dock.add(self.doneButton,DockPanel.SOUTH)
 
-        dock.add(HTML("Normal text box:"),DockPanel.CENTER)
+        dock.add(HTML(""),DockPanel.CENTER)
 
         self.setText("opkop")
         self.setPopupPosition(left, top)
         self.setStyleAttribute("background-color", "#ffffff")
-        self.setStyleAttribute("color", "white")
+        self.setStyleAttribute("color", "blue")
         self.setStyleAttribute("border-width", "5px")
         self.setStyleAttribute("border-style", "solid")
 
@@ -174,4 +175,6 @@ class FormulaBuilder(DialogWindow):
         if not self.formula.is_closed():
             self.formula.add_one_op(op,type)
             self.image.setUrl(latex_to_url(self.formula.fill_with_placeholders().to_latex()))
+            if self.formula.is_closed():
+                self.doneButton.setEnabled(True)
 
