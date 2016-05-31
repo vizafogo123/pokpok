15e4213cbca0b9816ee3f10d4c07b188e585aea6
Torma Róbert <tormarobert@Tormas-MacBook-Pro.local>
2016-05-31 14:48:38 +0200

    uzi

--- app/Root.py
+++ app/Root.py
@@ -6,26 +6,33 @@ from pyjamas.ui.RootPanel import RootPanel
 
 from app.FormulaBuilder import latex_to_url, FormulaBuilder
 from lion.Formula import Formula
-from lion.Operation import operations
-from lion.Theorem import axioms
+from lion.NormalForm import NormalForm
+from lion.Operation import Operation, operations
+from lion.Theorem import axioms, Theorem
 
 
 class Root():
     def __init__(self):
         self.current_theorem=None
         self.current_vars=[]
+        self.cnf=NormalForm([])
+        self.theorems=axioms
 
     def fill_combo_variable(self):
         self.combo_variable.clear()
         for var in self.current_vars:
             self.combo_variable.addItem(var.name)
 
+    def fill_combo_theorem(self):
+        self.combo_theorem.clear()
+        for theorem in self.theorems:
+            self.combo_theorem.addItem(theorem.name)
 
     def select_theorem(self):
-        self.current_theorem=axioms[self.combo_theorem.getSelectedIndex()]
+        self.current_theorem=self.theorems[self.combo_theorem.getSelectedIndex()]
         self.current_cnf=self.current_theorem.cnf.deepcopy()
         self.image_formula.setUrl(latex_to_url(self.current_theorem.formula.to_latex()))
-        self.image_cnf.setUrl(latex_to_url(self.current_cnf.to_latex()))
+        self.image_current_cnf.setUrl(latex_to_url(self.current_cnf.to_latex()))
 
         self.current_vars=self.current_cnf.get_vars()
         self.fill_combo_variable()
@@ -34,28 +41,49 @@ class Root():
         var=self.current_vars[self.combo_variable.getSelectedIndex()]
         def after(formula):
             self.current_cnf=self.current_cnf.substitute(Formula([var]),formula)
-            self.image_cnf.setUrl(latex_to_url(self.current_cnf.to_latex()))
+            self.image_current_cnf.setUrl(latex_to_url(self.current_cnf.to_latex()))
             del self.current_vars[self.combo_variable.getSelectedIndex()]
             self.fill_combo_variable()
 
-        a=FormulaBuilder(operations,after,type='expr')
+        a=FormulaBuilder([op for op in operations if op.available and op.type==Operation.EXPRESSION],after,type='expr')
         a.show()
 
+    def add_to_cnf(self):
+        self.cnf+=self.current_cnf
+        self.image_cnf.setUrl(latex_to_url(self.cnf.to_latex()))
+        if self.cnf.is_degenerate():
+            Window.alert("SADAT ABDEL")
+
+    def begin(self):
+        def after(formula):
+            self.theorems.append(Theorem(formula,'ind'))
+            self.fill_combo_theorem()
+
+        a=FormulaBuilder([op for op in operations if op.available],after,type='rel')
+        a.show()
+
+
     def start(self):
-        button1 = Button("Click me", self.select_theorem, StyleName='teststyle')
-        button2 = Button("Shari hao", self.substitute_variable, StyleName='teststyle')
+        self.button0 = Button("Begin", self.begin, StyleName='teststyle')
+        self.button1 = Button("Select theorem", self.select_theorem, StyleName='teststyle')
+        self.button2 = Button("Substitute variable", self.substitute_variable, StyleName='teststyle')
+        self.button3 = Button("Add", self.add_to_cnf, StyleName='teststyle')
 
         self.combo_theorem = ListBox(VisibleItemCount=1)
-        for ax in axioms:
-            self.combo_theorem.addItem(ax.name)
+        self.fill_combo_theorem()
 
         self.combo_variable = ListBox(VisibleItemCount=1)
         self.image_formula = Image()
+        self.image_current_cnf = Image()
         self.image_cnf = Image()
+
+        RootPanel().add(self.button0)
         RootPanel().add(self.combo_theorem)
-        RootPanel().add(button1)
+        RootPanel().add(self.button1)
         RootPanel().add(self.image_formula)
-        RootPanel().add(self.image_cnf)
         RootPanel().add(self.combo_variable)
-        RootPanel().add(button2)
+        RootPanel().add(self.button2)
+        RootPanel().add(self.image_current_cnf)
+        RootPanel().add(self.button3)
+        RootPanel().add(self.image_cnf)
 
