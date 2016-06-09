b1bdaf45617ee779bdd66f724a42e0e9fc0f1da8
Torma Róbert <tormarobert@Tormas-MacBook-Pro.local>
2016-06-09 10:26:28 +0200

    post

--- app/Root.py
+++ app/Root.py
@@ -7,10 +7,9 @@ from pyjamas.ui.RootPanel import RootPanel
 from app.FormulaBuilder import FormulaBuilder
 from app.TheoremApplier import TheoremApplier
 from app.Utils import fill_flextable_with_cnf
-from lion.NormalForm import NormalForm
-from lion.Operation import base_operations
-from lion.ProofState import global_proof_state, ProofState, ProofTreeItem
-from lion.Theorem import Theorem
+from lion.Formula import Formula
+from lion.Operation import base_operations, FORALL, A, NOT, IN
+from lion.ProofState import ProofState, ProofTreeItem
 
 
 class Root():
@@ -20,40 +19,41 @@ class Root():
     def start_proof(self):
         def after(formula):
             self.proof_root=ProofTreeItem.proof_root(formula=formula.negate())
-            self.proof_state=self.proof_root.proof_state
-            self.TheoremApplier.set_defaults(self.proof_state)
+            self.set_to_proof_tree_position(self.proof_root)
 
-        a = FormulaBuilder([op for op in base_operations if op.available], after, type='rel')
-        a.show()
+        # a = FormulaBuilder([op for op in base_operations if op.available], after, type='rel')
+        # a.show()
+
+        f=Formula([FORALL,A,NOT,IN,A,A])
+        after(f)
 
     def split_proof(self):
         def after(formula):
-            self.TheoremApplier.add_theorem(Theorem(formula, 'ind'))
+            self.proof_tree_position.split(formula)
+            self.set_to_proof_tree_position(self.proof_tree_position.children[0])
 
-        a = FormulaBuilder([op for op in self.proof_state.operations if op.available], after, type='rel')
+        a = FormulaBuilder([op for op in self.proof_tree_position.proof_state.operations if op.available], after, type='rel')
         a.show()
 
     def onReceivingCnf(self, cnf):
-        self.proof_state.cnf += cnf
+        self.proof_tree_position.proof_state.cnf += cnf
         self.refresh_cnf_table()
-        if self.proof_state.cnf.is_degenerate():
+        if self.proof_tree_position.proof_state.cnf.is_degenerate():
             Window.alert("SADAT ABDEL")
 
     def refresh_cnf_table(self):
-        fill_flextable_with_cnf(self.cnf_table, self.proof_state.cnf)
+        fill_flextable_with_cnf(self.cnf_table, self.proof_tree_position.proof_state.cnf)
 
-    def set_to_proof_state(self,proof_state):
-        self.proof_state = proof_state
-        self.TheoremApplier = TheoremApplier(self.proof_state, self.onReceivingCnf)
+    def set_to_proof_tree_position(self,proof_tree_position):
+        self.proof_tree_position = proof_tree_position
+        self.TheoremApplier.set_defaults(self.proof_tree_position.proof_state)
+        self.refresh_cnf_table()
 
     def start(self):
         button0 = Button("Begin", self.start_proof, StyleName='teststyle')
         button1 = Button("Split", self.split_proof, StyleName='teststyle')
 
-        self.proof_state = ProofState(theorems=global_proof_state.theorems, operations=global_proof_state.operations,
-                                      cnf=NormalForm([]))
-
-        self.TheoremApplier = TheoremApplier(self.proof_state, self.onReceivingCnf)
+        self.TheoremApplier = TheoremApplier(ProofState([]), self.onReceivingCnf)
 
         self.h = HorizontalPanel()
         self.h.setWidth("100%")
