b019db251af9dd1a6f6caca7523fc636f5c811bc
vizafogo123 <tormar23@gmail.com>
2017-06-10 22:27:46 +0200

    opka

---
+++ app/FormulaList.py
@@ -0,0 +1,28 @@
+from pyjamas import Window
+from pyjamas.ui.CheckBox import CheckBox
+from pyjamas.ui.HorizontalPanel import HorizontalPanel
+from pyjamas.ui.VerticalPanel import VerticalPanel
+
+from app.FormulaBuilder import latex_to_url, FormulaBuilder
+from pyjamas.ui.Button import Button
+from pyjamas.ui.Image import Image
+from pyjamas.ui.ListBox import ListBox
+
+
+class FormulaList(VerticalPanel):
+    def __init__(self):
+        VerticalPanel.__init__(self)
+        self.checkbox_list=[]
+
+    def add_fomula(self,f):
+        h=HorizontalPanel()
+        im=Image()
+        im.setUrl(latex_to_url(f.to_latex()))
+        c=CheckBox()
+        h.add(c)
+        h.add(im)
+        self.add(h)
+        self.checkbox_list+=[c]
+
+    def get_selected_indices(self):
+        return [i for i, c in enumerate(self.checkbox_list) if c.isChecked()]
